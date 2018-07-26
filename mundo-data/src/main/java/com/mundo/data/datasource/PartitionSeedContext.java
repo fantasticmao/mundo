056f6dee667eb925b04007721ffffce3b52d83ea
FantasticMao <maomao8017@gmail.com>
2018-07-26 18:26:18 +0800

    modified

--- mundo-data/src/main/java/com/mundo/data/datasource/PartitionSeedContext.java
+++ mundo-data/src/main/java/com/mundo/data/datasource/PartitionSeedContext.java
@@ -9,18 +9,15 @@ import java.util.Stack;
  * @since 2017/11/16
  */
 public class PartitionSeedContext implements AutoCloseable {
-    private static final ThreadLocal<Stack<Object>> SEED_STACK = new ThreadLocal<>();
+    private static final ThreadLocal<Stack<Object>> SEED_STACK = ThreadLocal.withInitial(Stack::new);
 
     public static Object push(Long seed) {
-        if (SEED_STACK.get() == null) {
-            SEED_STACK.set(new Stack<>());
-        }
         return SEED_STACK.get().push(seed);
     }
 
     public static Object pop() {
         Stack<Object> stack = SEED_STACK.get();
-        if (stack != null && !stack.empty()) {
+        if (!stack.empty()) {
             return stack.pop();
         }
         throw new PartitionException("seed stack is null or empty");
