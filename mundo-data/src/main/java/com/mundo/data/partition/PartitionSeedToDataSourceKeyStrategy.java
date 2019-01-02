662b4f2b5bd4347435d653758a980f0debeee20f
FantasticMao <maomao8017@gmail.com>
2019-01-02 18:13:21 +0800

    modified
    1. 调整 @PartitionParam 实现方式

--- mundo-data/src/main/java/com/mundo/data/partition/PartitionSeedToDataSourceKeyStrategy.java
+++ mundo-data/src/main/java/com/mundo/data/partition/PartitionSeedToDataSourceKeyStrategy.java
@@ -1,5 +1,6 @@
 package com.mundo.data.partition;
 
+import javax.annotation.Nullable;
 import java.util.function.Function;
 
 /**
@@ -11,4 +12,41 @@ import java.util.function.Function;
  */
 @FunctionalInterface
 public interface PartitionSeedToDataSourceKeyStrategy extends Function<Object, String> {
+
+    class NumberModulusStrategy implements PartitionSeedToDataSourceKeyStrategy {
+        private String formatStr;
+        private int dataSourceSize;
+
+        public NumberModulusStrategy(String formatStr, int dataSourceSize) {
+            this.formatStr = formatStr;
+            this.dataSourceSize = dataSourceSize;
+        }
+
+        @Override
+        public String apply(@Nullable Object seedObject) {
+            if (seedObject == null) { // 选择默认数据源
+                return null;
+            }
+
+            if (seedObject instanceof PartitionSeedProvider) {
+                seedObject = ((PartitionSeedProvider) seedObject).getSeed();
+            }
+
+            if (seedObject instanceof Number) {
+                Number seedNumber = (Number) seedObject;
+                return String.format(formatStr, (seedNumber.longValue() % dataSourceSize) + 1);
+            } else {
+                throw new IllegalArgumentException("cannot cast the partition seed object '" + seedObject + "' to 'java.lang.Number'");
+            }
+        }
+    }
+
+    class TimeRangeStrategy implements PartitionSeedToDataSourceKeyStrategy {
+
+        @Override
+        public String apply(Object o) {
+            // TODO 基于时间范围分库
+            return null;
+        }
+    }
 }
