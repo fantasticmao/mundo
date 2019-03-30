a4d9ebce874c9033b7f0e8d45024544de5b02ab1
FantasticMao <maomao8017@gmail.com>
2019-03-31 04:08:58 +0800

    new file
    1. 添加批量 install 脚本

---
+++ install.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+modules=('mundo-root' 'mundo-core' 'mundo-web' 'mundo-data')
+
+for module in ${modules[@]}
+do
+    echo "--------------------------< start install $module >--------------------------"
+    mvn clean install -Dmaven.test.skip=true -f $module/pom.xml -P aliyun
+    echo "---------------------------< end install $module >---------------------------"
+    echo -e "\n"
+done
