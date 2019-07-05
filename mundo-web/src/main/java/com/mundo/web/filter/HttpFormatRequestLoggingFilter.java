c747dda3351b0beb3006b0995d54a9f7c7a387ef
FantasticMao <maomao8017@gmail.com>
2019-07-05 22:08:22 +0800

    modified
    1. 修改 HttpFormatRequestLoggingFilter.java

--- mundo-web/src/main/java/com/mundo/web/filter/HttpFormatRequestLoggingFilter.java
+++ mundo-web/src/main/java/com/mundo/web/filter/HttpFormatRequestLoggingFilter.java
@@ -11,23 +11,48 @@ import java.util.Enumeration;
 
 /**
  * HttpFormatRequestLoggingFilter
+ * <p>
+ * 开启 HttpFormatRequestLoggingFilter 功能，需要在 logback.xml 中配置对应的 logger，示例配置如下：
+ * <pre>
+ *     <appender name="http_request_info" class="ch.qos.logback.core.rolling.RollingFileAppender">
+ *         <file>${log_home}/http-request.log</file>
+ *         <encoder>
+ *             <pattern>%date{yyyy-MM-dd HH:mm:ss.SSS} %-5level [%thread] %logger{5}:%L%n%message%n###%n%n</pattern>
+ *             <charset>UTF-8</charset>
+ *         </encoder>
+ *         <rollingPolicy class="ch.qos.logback.core.rolling.TimeBasedRollingPolicy">
+ *             <maxHistory>30</maxHistory>
+ *             <fileNamePattern>${log_home}/http-request.%d{yyyy-MM-dd}.log</fileNamePattern>
+ *         </rollingPolicy>
+ *     </appender>
+ *
+ *     <logger name="HttpFormatRequestLoggingFilter" level="TRACE" additivity="false">
+ *         <appender-ref ref="http_request_info"/>
+ *     </logger>
+ * </pre>
+ * </p>
  *
  * @author maomao
  * @since 2019-06-14
  */
 public class HttpFormatRequestLoggingFilter extends CommonsRequestLoggingFilter {
-    protected String loggerName = "HttpFormatRequestLoggingFilter";
-    private final Logger logger = LoggerFactory.getLogger(loggerName);
+    private final Logger logger;
 
     private static final String SPACE = Constant.Strings.SPACE;
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final int DEFAULT_MAX_PAYLOAD_LENGTH = 256;
+    private static final String DEFAULT_LOGGER_NAME = "HttpFormatRequestLoggingFilter";
 
     public HttpFormatRequestLoggingFilter() {
         this(DEFAULT_MAX_PAYLOAD_LENGTH);
     }
 
     public HttpFormatRequestLoggingFilter(int maxPayloadLength) {
+        this(maxPayloadLength, DEFAULT_LOGGER_NAME);
+    }
+
+    public HttpFormatRequestLoggingFilter(int maxPayloadLength, String loggerName) {
+        this.logger = LoggerFactory.getLogger(loggerName);
         maxPayloadLength = Math.max(maxPayloadLength, 1024);
         super.setMaxPayloadLength(maxPayloadLength);
     }
